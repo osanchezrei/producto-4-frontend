41bca694fed693b3f2cc541aafe68d419f2c263a
oriolSanchez <ori.reina@gmail.com>
2022-12-27 23:17:35 +0100

    native upgrade

--- App.js
+++ App.js
@@ -6,10 +6,29 @@ import { JugadorCarousel } from './src/pages/CarouselJugador';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ROUTES } from './src/router/routes';
+import React, { useState, useEffect } from 'react';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
   return (
     <View style={styles.container}>
       <NavigationContainer>
